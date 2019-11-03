feebb8077e15cd1de140d8d930ab5b74d81ee526
mase5 <mdewaegeneer@gmail.com>
2019-11-03 16:34:36 +0100

    Fix bug not working with more than 2 parameters. Bump to version 0.0.3.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pytinsel',
-    version='0.0.2',
+    version='0.0.3',
     author="Maxime De Waegeneer",
     author_email="mdewaegeneer@gmail.com",
     description="Decorators for Python",
