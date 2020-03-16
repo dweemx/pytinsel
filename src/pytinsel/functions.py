93b9167a4f3d46f867ac582d336c37370ed4ba9f
dweemx <mdewaegeneer@gmail.com>
2020-03-16 11:56:42 +0100

    Fix bug None in conditional statement with other strategy

--- src/pytinsel/functions.py
+++ src/pytinsel/functions.py
@@ -12,7 +12,6 @@ def hof(func):
         _LF = dis.opmap['LOAD_FAST'].to_bytes(1, byteorder='little')
         _PJIF = dis.opmap['POP_JUMP_IF_FALSE'].to_bytes(1, byteorder='little')
         _PJIT = dis.opmap['POP_JUMP_IF_TRUE'].to_bytes(1, byteorder='little')
-        _BS = dis.opmap['BUILD_STRING'].to_bytes(1, byteorder='little')
 
         byte_code = func.__code__.co_code
         _byte_code = b''
@@ -21,7 +20,10 @@ def hof(func):
             ith_byte = bytes([byte_code[i]])
             iplus1th_byte = bytes([byte_code[i + 1]])
             if ith_byte == _LC:
-                _byte_code = _byte_code + ith_byte + bytes([int.from_bytes(iplus1th_byte, 'little') + 1])
+                if iplus1th_byte in [b'\x00']:
+                    _byte_code = _byte_code + ith_byte + bytes([int.from_bytes(iplus1th_byte, 'little')])
+                else:
+                    _byte_code = _byte_code + ith_byte + bytes([int.from_bytes(iplus1th_byte, 'little') + 1])
             else:
                 _byte_code = _byte_code + ith_byte + iplus1th_byte
 
@@ -39,19 +41,12 @@ def hof(func):
         for i in range(0, len(__byte_code), 2):
             ith_byte = bytes([__byte_code[i]])
             iplus1th_byte = bytes([__byte_code[i + 1]])
-            if (i + 2) < len(__byte_code):
-                iplus2th_byte = bytes([__byte_code[i + 2]])
 
             if ith_byte == _LF and iplus1th_byte in [b'\x00']:
                 ___byte_code = ___byte_code + ith_byte + bytes([len(func.__code__.co_varnames) - 1])
             elif ith_byte == _SF and iplus1th_byte in [b'\x02'] and not _is_1st_param_stored:
                 ___byte_code = ___byte_code + ith_byte + bytes([len(func.__code__.co_varnames) - 1])
                 _is_1st_param_stored = True
-            elif ith_byte == _LC and _is_1st_param_stored:
-                if iplus2th_byte != _LF and iplus2th_byte != _SF and iplus2th_byte != _BS:
-                    ___byte_code = ___byte_code + ith_byte + bytes([int.from_bytes(iplus1th_byte, 'little') - 1])
-                else:
-                    ___byte_code = ___byte_code + ith_byte + bytes([int.from_bytes(iplus1th_byte, 'little')])
             elif ith_byte == _SF:
                 ___byte_code = ___byte_code + ith_byte + bytes([int.from_bytes(iplus1th_byte, 'little') - 1])
             elif ith_byte == _LF:
