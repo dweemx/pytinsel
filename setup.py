8a1ff4d28acd72df6a0e939492ace57d633f44f4
dweemx <mdewaegeneer@gmail.com>
2020-03-16 12:07:23 +0100

    Bump version from 0.0.16 to 0.0.17 Version from 0.0.11 to 0.0.16 were not working

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pytinsel',
-    version='0.0.16',
+    version='0.0.17',
     author="Maxime De Waegeneer",
     author_email="mdewaegeneer@gmail.com",
     description="Decorators for Python",
